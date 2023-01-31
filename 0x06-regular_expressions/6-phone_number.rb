97af0a4933148ce4b0bbaf020ddcae02ea9c362d
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-01-31 17:34:01 +0100

    Update 6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-## A regular expression that is matches 10 digit phone number
-#puts ARGV[0].scan(/^[0-9]{10}$/).join
+# A regular expression that is matches 10 digit phone number
+puts ARGV[0].scan(/^[0-9]{10}$/).join
