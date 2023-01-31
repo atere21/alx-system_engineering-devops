ba63c39b872be7751afce925706d87f5c582cbc0
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-01-31 17:40:57 +0100

    Update 7-OMG_WHY_ARE_YOU_SHOUTING.rb

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-## A regular expression that is matches only capital letters
-#puts ARGV[0].scan(/[A-Z]/).join
+# A regular expression that is matches only capital letters
+puts ARGV[0].scan(/[A-Z]/).join
