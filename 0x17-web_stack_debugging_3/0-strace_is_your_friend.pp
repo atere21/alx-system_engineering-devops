b945fa78085a9753ed3a8364b8427e568fbc4794
atere21 <tosinateret@gmail.com>
2023-05-09 04:13:48 -0600

    debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# change phpp to php script
+exec { 'fix-wordpress':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'sed -i -e "s/phpp/php/g" /var/www/html/wp-settings.php',
+  provider => 'shell',
+}
